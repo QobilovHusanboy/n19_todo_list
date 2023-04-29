843270765d2fee9d003680e8adf14f5ccca88f5a
Husanboy Qobilov <husanboyqobilov83@gmail.com>
2023-04-29 15:07:53 +0500

    Create Database

--- queries.sql
+++ queries.sql
@@ -9,5 +9,26 @@ CREATE TABLE IF NOT EXISTS tasks(
     id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     task VARCHAR(128) NOT NULL, 
     info VARCHAR(512) ,
-    parent_id INT 
+    parent_id INT,
+    is_done TINYINT DEFAULT 0,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+INSERT INTO tasks (task, info,parent_id) VALUES 
+    ("DO HOMEWPRK","I have a lot  of tasks to do",0),
+    ("TASK 1 "," ",1),
+    ("TASK 2 "," ",1),
+    ("TASK 3 "," ",1),
+    ("TASK 4 "," ",1);
+
+INSERT INTO tasks(task,parent_id) VALUES
+    ("Download CS.GO",0),
+    ("Update system",0);
+
+
+INSERT INTO tasks(task,info, parent_id,is_done) VALUES
+    ("Prepare OS","Install all additional soft on it",0,1),
+    ("Install Office app","Download it from microsoft.com",7,1),
+    ("Install Google Chrome","Make as primary browser",7,1);
